412ccb505e3d2e293f596166b951f1702495fcaf
JoseMtz02 <josemtzlara@hotmail.com>
2025-10-06 15:03:59 -0500

    configuracion cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import cors from "cors";
 import { pool_mssql } from "./connection/mssql";
 import { connectMongo } from "./connection/mongo";
 import router from "./routes/route";
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 app.use("/api", router);
